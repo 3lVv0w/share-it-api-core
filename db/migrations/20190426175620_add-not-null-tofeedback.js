09db09dae62e8ea249d12c92e5f8221ca9eb531a
Unknown <prut238@gmail.com>
2019-04-26 18:03:11 +0700

    add not null to feedback

---
+++ db/migrations/20190426175620_add-not-null-tofeedback.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.table('feedback', function (table) {
+     
+        table.string('comment').notNull().defaultTo(' ').alter();
+     
+    });
+    
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.table('feedback', function (table) {
+ 
+        table.dropColumn('comment').notNull().defaultTo(' ').alter();
+
+        
+    });
+}
