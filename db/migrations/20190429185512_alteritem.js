95a5b464d95156f042bfa0460f2ef93d744b1e77
Unknown <prut238@gmail.com>
2019-04-29 19:05:07 +0700

    cange db

---
+++ db/migrations/20190429185512_alteritem.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.table('request', function (table) {
+    
+        table.string('borrow_time').notNull().alter();
+        table.string('return_time').notNull().alter();
+     
+    });
+    
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.table('request', function (table) {
+        table.dropColumn('borrow_time').notNull().alter();
+        table.dropColumn('return_time').notNull().alter();
+     
+    });
+}
---
+++ db/migrations/20190429185826_alteritem.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.table('session', function (table) {
+     
+        table.string('start_time').notNull().alter();
+        table.string('end_time').notNull().alter();
+    });
+    
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.table('session', function (table) {
+        table.dropColumn('start_time').notNull().alter();
+        table.dropColumn('end_time').notNull().alter();
+       
+    });
+}
